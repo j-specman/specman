793469c0f9abc1b248ac2272fcc7937af6d6e7c3
Jan Lessner <jan.lessner@sn-invent.de>
2026-05-28 15:08:05 +0200

    multiple change sets - consistent terms "reject" and "accept".

--- src/main/java/specman/view/StepRemovalPurpose.java
+++ src/main/java/specman/view/StepRemovalPurpose.java
@@ -4,7 +4,8 @@ package specman.view;
  * The appropriate removal methods might need to know that. E.g. a break step with a linked
  * catch sequence, will also discard this sequence in case it is discarded but keep it in case of
  * movement.
- * Another reason for step removal might be rejecting or accepting a change set. */
+ * Another reason for step removal might be rejecting or accepting a change set. This is important
+ * to know because in these cases we allow temporary inconsistencies during the change set clearance. */
 public enum StepRemovalPurpose {
   Discard,
   Move,
